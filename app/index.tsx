9995ea55f2f6a2db92277bd132f3b4791297df51
bombh <fabrice@bxxl.be>
2024-08-01 21:56:56 +0200

    Supabase & Clerk init

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,15 @@
-import { Text, View } from "react-native";
+import { Text, View } from "react-native"
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Home.</Text>
-    </View>
-  );
+   return (
+      <View
+         style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+         }}
+      >
+         <Text>Home. SUPABASE=E1mw1TZkcaXPDhEJ</Text>
+      </View>
+   )
 }
